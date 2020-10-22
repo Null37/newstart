4e95aae96b39f1d283eb78cbbbee0bf462c0f2b8
saad allah samadi <42317207+zerossamadi@users.noreply.github.com>
2020-10-22 08:34:00 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@
 service nginx $1 && service mysql $1 && service php7.3-fpm $1
 	mysql -u root -e "CREATE USER 'null'@'localhost' IDENTIFIED BY 'root';"
 	mysql -u root -e "CREATE DATABASE wordpress"
+	mysql -u root -e "CREATE DATABASE phpmyadmin"
 	mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'null'@'localhost' IDENTIFIED BY 'root';"
 	mysql -u root wordpress < wordpress.sql
 	mysql -u root phpmyadmin < phpmyadmin.sql
